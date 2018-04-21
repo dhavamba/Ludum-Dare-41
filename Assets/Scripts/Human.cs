7d772d27ee075680388832f832a1c976791011be
MSI\dhava <dhavamba@gmail.com>
2018-04-21 15:06:28 +0100

    added humans

---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+
+    public string completeName;
+
+    void OnMouseDown()
+    {
+        Debug.Log("ti ho preso");
+    }
+}
